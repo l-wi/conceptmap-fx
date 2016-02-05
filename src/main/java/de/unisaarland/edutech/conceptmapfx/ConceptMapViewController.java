1c972984acff6e62efb0f3fec656fa4cec7cc583
Tim Steuer <steuertim@gmail.com>
2016-02-05 13:19:07 +0100

    Moved the creation of a conceptView into a builder class

--- src/main/java/de/unisaarland/edutech/conceptmapfx/ConceptMapViewController.java
+++ src/main/java/de/unisaarland/edutech/conceptmapfx/ConceptMapViewController.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import de.unisaarland.edutech.conceptmapfx.InputViewController.Position;
 import de.unisaarland.edutech.conceptmapfx.event.ConceptDeletedListener;
+import de.unisaarland.edutech.conceptmapfx.event.ConceptEditRequestedListener;
 import de.unisaarland.edutech.conceptmapfx.event.ConceptMovedListener;
 import de.unisaarland.edutech.conceptmapfx.event.LinkDeletedListener;
 import de.unisaarland.edutech.conceptmapfx.event.LinkDirectionUpdatedListener;
@@ -78,23 +79,24 @@ public class ConceptMapViewController implements NewLinkListener, NewConceptList
 	}
 
 	public void newConcept(InputViewController inputViewController) {
-		try {
+	
+		User user = inputViewController.getUser();
+		Optional<ConceptViewController> emptyConceptViewController = nextEmptyConcept(user);
 
-			// check if there is already an empty concept for the user
-			User user = inputViewController.getUser();
-			Optional<ConceptViewController> emptyConceptViewController = nextEmptyConcept(user);
-
-			if (emptyConceptViewController.isPresent()) {
-				LOG.warn("there is still already an empty concept for user" + user);
-				emptyConceptViewController.get().highlightEmpty();
-				return;
-			}
-
-			newConceptInUIAndLogic(inputViewController, user);
-
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+		if (emptyConceptViewController.isPresent()) {
+			LOG.warn("there is still already an empty concept for user" + user);
+			emptyConceptViewController.get().highlightEmpty();
+			return;
 		}
+
+		ConceptViewBuilder builder = new ConceptViewBuilder(this.conceptMap);
+		builder = builder.withNewConcept(user).withMovedListener(this);
+		
+		for (ConceptEditRequestedListener l : inputControllers)
+			builder.withEditRequestedListener(l);
+		
+		ConceptViewController controller = builder.buildControllerAndAddView(inputViewController, this.conceptMapPane);
+		this.userToConceptViewControllers.get(user).add(controller);
 	}
 
 	public void newLink(ConceptViewController cv1, ConceptViewController cv2) {
@@ -131,96 +133,11 @@ public class ConceptMapViewController implements NewLinkListener, NewConceptList
 	}
 
 	private Optional<ConceptViewController> nextEmptyConcept(User user) {
-		
+
 		for (ConceptViewController controller : userToConceptViewControllers.get(user))
 			if (controller.getConcept().getName().getContent().equals(""))
 				return Optional.of(controller);
 		return Optional.ofNullable(null);
-	}
-
-	private Concept initConcept(InputViewController inputViewController) {
-		User owner = inputViewController.getUser();
-		Concept concept = new Concept(new CollaborativeString(owner));
-		conceptMap.addConcept(concept);
-		return concept;
-	}
-
-	private ConceptViewController initConceptViewController(Concept concept, FXMLLoader loader) {
-		ConceptViewController conceptViewController = loader.getController();
-
-		conceptViewController.setConcept(concept);
-		conceptViewController.setParticipants(conceptMap.getExperiment().getParticipants());
-
-		for (InputViewController inputController : inputControllers) {
-			conceptViewController.addConceptEditRequestedListener(inputController);
-		}
-		conceptViewController.addConceptMovedListener(this);
-
-		return conceptViewController;
-	}
-
-	private void initConceptViewUI(InputViewController inputViewController, FXMLLoader loader) throws IOException {
-
-		Pane conceptViewPane = loader.load();
-		// force the conceptMapPane to calculate the size of the conceptViewPane
-		conceptMapPane.getChildren().add(conceptViewPane);
-		conceptMapPane.applyCss();
-		conceptMapPane.layout(); // TODO load all the things that might already
-									// exit in that concept map
-		// to UI
-
-		moveConceptToRightPosition(inputViewController, conceptViewPane);
-	}
-
-	private void moveConceptToRightPosition(InputViewController inputViewController, Pane conceptViewPane) {
-		// TODO what if current space is occupied
-
-		Point2D p = new Point2D(0, -50);
-		Point2D pScene = inputViewController.transformLocalToScene(p);
-
-		Pane n = conceptViewPane;
-
-		double x = pScene.getX();
-		double y = pScene.getY();
-
-		if (inputViewController.getPosition() == Position.NORTH) {
-			x = x - n.getWidth();
-		} else if (inputViewController.getPosition() == Position.WEST) {
-			x = x - n.getWidth() / 2 + n.getHeight() / 2;
-			y = y + n.getWidth() / 2 - n.getHeight() / 2;
-		} else if (inputViewController.getPosition() == Position.EAST) {
-			x = x - n.getWidth() / 2 - n.getHeight() / 2;
-			y = y - n.getWidth() / 2 - n.getHeight() / 2;
-		} else if (inputViewController.getPosition() == Position.SOUTH) {
-			y = y - n.getHeight();
-		}
-
-		conceptViewPane.setTranslateX(x);
-		conceptViewPane.setTranslateY(y);
-
-		conceptViewPane.setRotate(inputViewController.getRotate());
-
-	}
-
-	private void newConceptInUIAndLogic(InputViewController inputViewController, User user) throws IOException {
-		// init logic
-		Concept concept = initConcept(inputViewController);
-
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("ConceptView.fxml"));
-
-		// load and init UI
-		initConceptViewUI(inputViewController, loader);
-
-		// init UI event-logic
-		ConceptViewController conceptViewController = initConceptViewController(concept, loader);
-		requestInput(concept.getOwner(), conceptViewController);
-
-		this.userToConceptViewControllers.get(user).add(conceptViewController);
-	}
-
-	private void requestInput(User owner, ConceptViewController conceptViewController) {
-		LOG.info("requesting input for user:\t" + owner + " on new concept!");
-		conceptViewController.setUserEnabled(owner, true);
 	}
 
 	@Override
---
+++ src/main/java/de/unisaarland/edutech/conceptmapfx/ConceptViewBuilder.java
@@ -0,0 +1,110 @@
+package de.unisaarland.edutech.conceptmapfx;
+
+import java.io.IOException;
+
+import de.unisaarland.edutech.conceptmapfx.InputViewController.Position;
+import de.unisaarland.edutech.conceptmapfx.event.ConceptEditRequestedListener;
+import de.unisaarland.edutech.conceptmapfx.event.ConceptMovedListener;
+import de.unisaarland.edutech.conceptmapping.CollaborativeString;
+import de.unisaarland.edutech.conceptmapping.Concept;
+import de.unisaarland.edutech.conceptmapping.ConceptMap;
+import de.unisaarland.edutech.conceptmapping.User;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
+import javafx.scene.layout.Pane;
+
+public class ConceptViewBuilder {
+
+	private ConceptMap map;
+
+	private Pane conceptViewPane;
+
+	private ConceptViewController controller;
+
+	public ConceptViewBuilder(ConceptMap map) {
+		try {
+			this.map = map;
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("ConceptView.fxml"));
+			this.conceptViewPane = loader.load();
+			this.controller = loader.getController();
+			this.controller.setParticipants(map.getExperiment().getParticipants());
+		} catch (IOException e) {
+			//should never happen fxml error
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public ConceptViewController buildControllerAndAddView(InputViewController positionOver, Pane conceptMapPane) {
+
+		conceptMapPane.getChildren().add(conceptViewPane);
+		conceptMapPane.applyCss();
+		conceptMapPane.layout();
+
+		moveConceptToRightPosition(positionOver, conceptViewPane);
+
+		requestInput(controller);
+		return controller;
+	}
+
+	public ConceptViewBuilder forConcept(Concept c) {
+		controller.setConcept(c);
+		return this;
+	}
+
+	public ConceptViewBuilder withNewConcept(User u) {
+		Concept c = initConcept(u);
+		return forConcept(c);
+	}
+
+	public ConceptViewBuilder withEditRequestedListener(ConceptEditRequestedListener l) {
+		controller.addConceptEditRequestedListener(l);
+		return this;
+	}
+
+	public ConceptViewBuilder withMovedListener(ConceptMovedListener l) {
+		controller.addConceptMovedListener(l);
+		return this;
+	}
+
+	private Concept initConcept(User u) {
+		Concept concept = new Concept(new CollaborativeString(u));
+		map.addConcept(concept);
+		return concept;
+	}
+
+	private void requestInput(ConceptViewController conceptViewController) {
+		conceptViewController.setUserEnabled(controller.getConcept().getOwner(), true);
+	}
+
+	private void moveConceptToRightPosition(InputViewController inputViewController, Pane conceptViewPane) {
+		// TODO what if current space is occupied
+
+		Point2D p = new Point2D(0, -50);
+		Point2D pScene = inputViewController.transformLocalToScene(p);
+
+		Pane n = conceptViewPane;
+
+		double x = pScene.getX();
+		double y = pScene.getY();
+
+		if (inputViewController.getPosition() == Position.NORTH) {
+			x = x - n.getWidth();
+		} else if (inputViewController.getPosition() == Position.WEST) {
+			x = x - n.getWidth() / 2 + n.getHeight() / 2;
+			y = y + n.getWidth() / 2 - n.getHeight() / 2;
+		} else if (inputViewController.getPosition() == Position.EAST) {
+			x = x - n.getWidth() / 2 - n.getHeight() / 2;
+			y = y - n.getWidth() / 2 - n.getHeight() / 2;
+		} else if (inputViewController.getPosition() == Position.SOUTH) {
+			y = y - n.getHeight();
+		}
+
+		conceptViewPane.setTranslateX(x);
+		conceptViewPane.setTranslateY(y);
+
+		conceptViewPane.setRotate(inputViewController.getRotate());
+
+	}
+
+}
