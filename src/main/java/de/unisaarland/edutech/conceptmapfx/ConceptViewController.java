9a09c5bf3e091bfd278f3d65f226f030102c22d8
Tim Steuer <steuer.tim@gmail.com>
2016-02-09 10:14:39 +0100

    rotation via touch

--- src/main/java/de/unisaarland/edutech/conceptmapfx/ConceptViewController.java
+++ src/main/java/de/unisaarland/edutech/conceptmapfx/ConceptViewController.java
@@ -28,6 +28,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.RotateEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.input.TouchEvent;
 import javafx.scene.input.TouchPoint;
@@ -94,163 +95,6 @@ public class ConceptViewController implements ConceptMovingListener, InputClosed
 		cv.translate(x, y);
 	}
 
-	public Bounds getBoundsInScene() {
-		return conceptPane.getLocalToSceneTransform().transform(conceptPane.getBoundsInLocal());
-	}
-
-	public Point2D getCenterAsSceneCoordinates() {
-		Point2D p = new Point2D(conceptPane.getWidth() / 2, conceptPane.getHeight() / 2);
-		return conceptPane.getLocalToSceneTransform().transform(p);
-	}
-
-	public Concept getConcept() {
-		return concept;
-	}
-
-	public double getHeight() {
-		return conceptPane.getHeight();
-	}
-
-	public double getRotate() {
-		return conceptPane.getRotate();
-	}
-
-	public double getWidth() {
-		return conceptPane.getWidth();
-	}
-
-	public void highlightEmpty() {
-		FadeTransition ft = new FadeTransition(Duration.millis(300), conceptPane);
-		ft.setFromValue(1.0);
-		ft.setToValue(0.5);
-		ft.setAutoReverse(true);
-		ft.setCycleCount(2);
-		ft.play();
-
-	}
-
-	@FXML
-	public void initialize() {
-
-		this.addConceptMovingListener(this);
-
-		this.inputToggleGroup = new InputToggleGroup(this, btnToogleUser1, btnToogleUser2, btnToogleUser3,
-				btnToogleUser4);
-
-		constructResizableTextfield(txtConcept);
-		
-		txtConcept.caretPositionProperty().addListener((c, o, n) -> {
-			this.editable.setCaretPosition(n.intValue());
-
-		});
-
-	}
-
-	@FXML
-	public void onRotate(ScrollEvent l) {
-		this.rotate(this.getRotate() + l.getDeltaY() / 40);
-		fireConceptMoving(0, 0, this.getRotate() + l.getDeltaY() / 40, this, null);
-	}
-
-	@FXML
-	public void onMouseMoving(MouseEvent evt) {
-		this.fireConceptMoving(evt.getX() - dragX, evt.getY() - dragY, conceptPane.getRotate(), this, null);
-	}
-
-	@FXML
-	public void onTouchMoving(TouchEvent evt) {
-		TouchPoint p = evt.getTouchPoint();
-		this.fireConceptMoving(p.getX() - dragX, p.getY() - dragY, conceptPane.getRotate(), this, null);
-		evt.consume();
-	}
-
-	@FXML
-	public void onMouseMoved(MouseEvent evt) {
-		this.fireConceptMoved();
-	}
-
-	@FXML
-	public void onTouchMoved(TouchEvent evt) {
-		this.fireConceptMoved();
-		evt.consume();
-	}
-
-	@FXML
-	public void onMouseMovingStarted(MouseEvent evt) {
-		movingStarted(evt.getX(), evt.getY());
-	}
-
-	@FXML
-	public void onTouchMovingStarted(TouchEvent evt) {
-		movingStarted(evt.getTouchPoint().getX(), evt.getTouchPoint().getY());
-		evt.consume();
-	}
-
-	private void movingStarted(double x, double y) {
-		this.dragX = x;
-		this.dragY = y;
-	}
-
-	public void inputClosed(User u) {
-		inputToggleGroup.setUserEnabled(participants.indexOf(u), false);
-	}
-
-	public boolean intersects(ConceptViewController other) {
-		Bounds myParentBounds = this.conceptPane.getBoundsInParent();
-		Bounds otherParentBounds = other.conceptPane.getBoundsInParent();
-
-		return myParentBounds.intersects(otherParentBounds);
-	}
-
-	public void rotate(double d) {
-		conceptPane.setRotate(d);
-	}
-
-	public void setConcept(Concept concept) {
-		this.concept = concept;
-		this.editable = new Editable(concept.getName(), txtConcept);
-
-	}
-
-	public void setParticipants(List<User> participants) {
-		this.participants = participants;
-	}
-
-	public void setUserEnabled(User owner, boolean b) {
-		inputToggleGroup.setUserEnabled(participants.indexOf(owner), b);
-	}
-
-	public void translate(double x, double y) {
-		double rotation = Math.toRadians(conceptPane.getRotate());
-
-		// if we have a rotation we have to convert to the rotated coordinate
-		// system
-		double xRotated = x * Math.cos(rotation) - y * Math.sin(rotation);
-		double yRotated = x * Math.sin(rotation) + y * Math.cos(rotation);
-		translateRelative(xRotated, yRotated);
-	}
-
-	private void translateRelative(double xRotated, double yRotated) {
-		translateAbsolute(conceptPane.getTranslateX() + xRotated, conceptPane.getTranslateY() + yRotated);
-	}
-
-	public void translateAbsolute(double x, double y) {
-		conceptPane.setTranslateX(x);
-		conceptPane.setTranslateY(y);
-	}
-
-	public void userToggleEnabled(int buttonID) {
-		this.txtConcept.setDisable(false);
-		this.fireEditRequested(participants.get(buttonID));
-	}
-
-	public Point2D getOrigin() {
-		double x = this.conceptPane.getLayoutX() + conceptPane.getTranslateX();
-		double y = this.conceptPane.getLayoutY() + conceptPane.getTranslateY();
-		return new Point2D(x, y);
-
-	}
-
 	private void constructResizableTextfield(TextField txt) {
 		txt.setMaxWidth(Region.USE_PREF_SIZE);
 		txt.textProperty().addListener((ov, prevText, currText) -> {
@@ -293,8 +137,135 @@ public class ConceptViewController implements ConceptMovingListener, InputClosed
 		conceptEditListeners.forEach(l -> l.conceptEditRequested(this, this.editable, u));
 	}
 
-	public void setRotate(double rotate) {
-		this.conceptPane.setRotate(rotate);
+	public Bounds getBoundsInScene() {
+		return conceptPane.getLocalToSceneTransform().transform(conceptPane.getBoundsInLocal());
+	}
+
+	public Point2D getCenterAsSceneCoordinates() {
+		Point2D p = new Point2D(conceptPane.getWidth() / 2, conceptPane.getHeight() / 2);
+		return conceptPane.getLocalToSceneTransform().transform(p);
+	}
+
+	public Concept getConcept() {
+		return concept;
+	}
+
+	public double getHeight() {
+		return conceptPane.getHeight();
+	}
+
+	public Point2D getOrigin() {
+		double x = this.conceptPane.getLayoutX() + conceptPane.getTranslateX();
+		double y = this.conceptPane.getLayoutY() + conceptPane.getTranslateY();
+		return new Point2D(x, y);
+
+	}
+
+	public double getRotate() {
+		return conceptPane.getRotate();
+	}
+
+	public double getWidth() {
+		return conceptPane.getWidth();
+	}
+
+	public ReadOnlyDoubleProperty heightProperty() {
+		return this.conceptPane.heightProperty();
+	}
+
+	public void highlightEmpty() {
+		FadeTransition ft = new FadeTransition(Duration.millis(300), conceptPane);
+		ft.setFromValue(1.0);
+		ft.setToValue(0.5);
+		ft.setAutoReverse(true);
+		ft.setCycleCount(2);
+		ft.play();
+
+	}
+
+	@FXML
+	public void initialize() {
+
+		this.addConceptMovingListener(this);
+
+		this.inputToggleGroup = new InputToggleGroup(this, btnToogleUser1, btnToogleUser2, btnToogleUser3,
+				btnToogleUser4);
+
+		constructResizableTextfield(txtConcept);
+
+		txtConcept.caretPositionProperty().addListener((c, o, n) -> {
+			this.editable.setCaretPosition(n.intValue());
+
+		});
+
+	}
+
+	public void inputClosed(User u) {
+		inputToggleGroup.setUserEnabled(participants.indexOf(u), false);
+	}
+
+	public boolean intersects(ConceptViewController other) {
+		Bounds myParentBounds = this.conceptPane.getBoundsInParent();
+		Bounds otherParentBounds = other.conceptPane.getBoundsInParent();
+
+		return myParentBounds.intersects(otherParentBounds);
+	}
+
+	private void movingStarted(double x, double y) {
+		this.dragX = x;
+		this.dragY = y;
+	}
+
+	@FXML
+	public void onMouseMoved(MouseEvent evt) {
+		this.fireConceptMoved();
+	}
+
+	@FXML
+	public void onMouseMoving(MouseEvent evt) {
+		this.fireConceptMoving(evt.getX() - dragX, evt.getY() - dragY, conceptPane.getRotate(), this, null);
+	}
+
+	@FXML
+	public void onMouseMovingStarted(MouseEvent evt) {
+		movingStarted(evt.getX(), evt.getY());
+	}
+
+	@FXML
+	public void onMouseRotate(ScrollEvent l) {
+		double rotation = l.getDeltaY() / 40;
+		if (l.getTouchCount() == 0)
+			onRotate(rotation);
+
+	}
+
+	@FXML
+	public void onTouchRotate(RotateEvent e) {
+		onRotate(e.getAngle());
+	}
+
+	private void onRotate(double rotation) {
+		this.rotate(this.getRotate() + rotation);
+		fireConceptMoving(0, 0, this.getRotate() + rotation, this, null);
+	}
+
+	@FXML
+	public void onTouchMoved(TouchEvent evt) {
+		this.fireConceptMoved();
+		evt.consume();
+	}
+
+	@FXML
+	public void onTouchMoving(TouchEvent evt) {
+		TouchPoint p = evt.getTouchPoint();
+		this.fireConceptMoving(p.getX() - dragX, p.getY() - dragY, conceptPane.getRotate(), this, null);
+		evt.consume();
+	}
+
+	@FXML
+	public void onTouchMovingStarted(TouchEvent evt) {
+		movingStarted(evt.getTouchPoint().getX(), evt.getTouchPoint().getY());
+		evt.consume();
 	}
 
 	@FXML
@@ -303,13 +274,47 @@ public class ConceptViewController implements ConceptMovingListener, InputClosed
 	}
 
 	@FXML
+	public void onTxtMouseReleased(MouseEvent evt) {
+		onTxtReleased();
+
+	}
+
+	private void onTxtReleased() {
+		showTools(false);
+		this.fireConceptMoved();
+
+	}
+
+	@FXML
 	public void onTxtTouchPressed(TouchEvent evt) {
 		txtPressed(evt.getTouchPoint().getX(), evt.getTouchPoint().getY());
 	}
 
-	private void txtPressed(double x, double y) {
-		showTools(true);
-		movingStarted(x, y);
+	@FXML
+	public void onTxtTouchReleased(TouchEvent evt) {
+		onTxtReleased();
+	}
+
+	public void rotate(double d) {
+		conceptPane.setRotate(d);
+	}
+
+	public void setConcept(Concept concept) {
+		this.concept = concept;
+		this.editable = new Editable(concept.getName(), txtConcept);
+
+	}
+
+	public void setParticipants(List<User> participants) {
+		this.participants = participants;
+	}
+
+	public void setRotate(double rotate) {
+		this.conceptPane.setRotate(rotate);
+	}
+
+	public void setUserEnabled(User owner, boolean b) {
+		inputToggleGroup.setUserEnabled(participants.indexOf(owner), b);
 	}
 
 	private void showTools(boolean b) {
@@ -328,28 +333,36 @@ public class ConceptViewController implements ConceptMovingListener, InputClosed
 
 	}
 
-	@FXML
-	public void onTxtMouseReleased(MouseEvent evt) {
-		onTxtReleased();
+	public void translate(double x, double y) {
+		double rotation = Math.toRadians(conceptPane.getRotate());
 
+		// if we have a rotation we have to convert to the rotated coordinate
+		// system
+		double xRotated = x * Math.cos(rotation) - y * Math.sin(rotation);
+		double yRotated = x * Math.sin(rotation) + y * Math.cos(rotation);
+		translateRelative(xRotated, yRotated);
 	}
 
-	@FXML
-	public void onTxtTouchReleased(TouchEvent evt) {
-		onTxtReleased();
+	public void translateAbsolute(double x, double y) {
+		conceptPane.setTranslateX(x);
+		conceptPane.setTranslateY(y);
 	}
 
-	private void onTxtReleased() {
-		showTools(false);
-		this.fireConceptMoved();
+	private void translateRelative(double xRotated, double yRotated) {
+		translateAbsolute(conceptPane.getTranslateX() + xRotated, conceptPane.getTranslateY() + yRotated);
+	}
 
+	private void txtPressed(double x, double y) {
+		showTools(true);
+		movingStarted(x, y);
+	}
+
+	public void userToggleEnabled(int buttonID) {
+		this.txtConcept.setDisable(false);
+		this.fireEditRequested(participants.get(buttonID));
 	}
 
 	public ReadOnlyDoubleProperty widthProperty() {
 		return this.conceptPane.widthProperty();
-	}
-
-	public ReadOnlyDoubleProperty heightProperty() {
-		return this.conceptPane.heightProperty();
 	}
 }
