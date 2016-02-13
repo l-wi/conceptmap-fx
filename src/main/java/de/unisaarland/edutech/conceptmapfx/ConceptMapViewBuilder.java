f97b809e05e6899230244795950f59f26a5e2ce2
Tim Steuer <steuertim@gmail.com>
2016-02-13 23:03:06 +0100

    moved setup code of main to own builder

---
+++ src/main/java/de/unisaarland/edutech/conceptmapfx/ConceptMapViewBuilder.java
@@ -0,0 +1,118 @@
+package de.unisaarland.edutech.conceptmapfx;
+
+import java.io.IOException;
+import java.util.List;
+
+import de.unisaarland.edutech.conceptmapfx.InputViewController.Position;
+import de.unisaarland.edutech.conceptmapping.ConceptMap;
+import de.unisaarland.edutech.conceptmapping.User;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+
+public class ConceptMapViewBuilder {
+
+	private Pane conceptMapView;
+	private ConceptMapViewController controller;
+	private Scene scene;
+
+	public ConceptMapViewBuilder() {
+		try {
+			FXMLLoader conceptMapLoader = new FXMLLoader(getClass().getResource("ConceptMapView.fxml"));
+
+			conceptMapView = conceptMapLoader.load();
+			controller = conceptMapLoader.getController();
+			scene = new Scene(conceptMapView);
+			controller.addNewLinkListener(controller);
+
+			controller.sceneWidthProperty().bind(scene.widthProperty());
+			controller.sceneHeightProperty().bind(scene.heightProperty());
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public Scene build() {
+		return scene;
+	}
+
+	public ConceptMapViewBuilder withConceptMap(ConceptMap conceptMap) {
+		controller.setConceptMap(conceptMap);
+		withParticipants(conceptMap.getExperiment().getParticipants());
+		return this;
+	}
+
+	private ConceptMapViewBuilder withParticipants(List<User> participants) {
+		try {
+			setInputPositions(participants.get(0), participants.get(1), participants.get(2), participants.get(3));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		return this;
+
+	}
+
+	private void setInputPositions(User u1, User u2, User u3, User u4) throws IOException {
+		// north
+		final Pane v1 = addInputComponent(Position.NORTH, u1);
+		v1.setRotate(180);
+		scene.widthProperty().addListener((observeable, oldVal, newVal) -> {
+			v1.setLayoutX(newVal.doubleValue() * 0.6 - v1.getWidth() / 2);
+		});
+
+		// west
+		final Pane v2 = addInputComponent(Position.WEST, u2);
+		v2.setRotate(90);
+
+		// as we rotate around center, we need to readjust on screen
+		scene.widthProperty().addListener(c -> {
+			v2.setTranslateX(-(v2.getWidth() / 2 - v2.getHeight() / 2));
+		});
+
+		scene.heightProperty().addListener((observeable, oldVal, newVal) -> {
+			v2.setLayoutY(newVal.doubleValue() * 0.5);
+		});
+
+		// south
+		final Pane v3 = addInputComponent(Position.SOUTH, u3);
+		scene.widthProperty().addListener((observeable, oldVal, newVal) -> {
+			v3.setLayoutX(newVal.doubleValue() * 0.5 - (v3.getWidth() / 2));
+		});
+
+		scene.heightProperty().addListener((observeable, oldVal, newVal) -> {
+			v3.setLayoutY(newVal.doubleValue() - v3.getHeight());
+		});
+
+		// east
+		final Pane v4 = addInputComponent(Position.EAST, u4);
+		v4.setRotate(270);
+		// as we rotate around center, we need to readjust on screen
+		scene.heightProperty().addListener(c -> {
+			v4.setTranslateX((v4.getWidth() / 2 - v4.getHeight() / 2));
+		});
+		scene.widthProperty().addListener((observeable, oldVal, newVal) -> {
+			v4.setLayoutX(newVal.doubleValue() - v4.getWidth());
+
+		});
+
+		scene.heightProperty().addListener((observeable, oldVal, newVal) -> {
+			v4.setLayoutY(newVal.doubleValue() * 0.5);
+		});
+
+	}
+
+	private Pane addInputComponent(InputViewController.Position p, User u) throws IOException {
+		FXMLLoader inputLoader = new FXMLLoader(Main.class.getResource("InputView.fxml"));
+		Pane inputView = inputLoader.load();
+		InputViewController inputController = inputLoader.getController();
+		inputController.setUser(u);
+		inputController.setPosition(p);
+		conceptMapView.getChildren().add(inputView);
+		inputController.addNewConceptListener(controller);
+		controller.addInputViewController(inputController);
+		return inputView;
+	}
+}
--- src/main/java/de/unisaarland/edutech/conceptmapfx/Main.java
+++ src/main/java/de/unisaarland/edutech/conceptmapfx/Main.java
@@ -20,8 +20,6 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	private ConceptMapViewController conceptMapController;
-
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -55,107 +53,22 @@ public class Main extends Application {
 		loss.setY(0.664);
 		loss.setRotate(0);
 		conceptMap.addConcept(loss);
-		//
-
-		// Link causes = conceptMap.addDirectedLink(lightsaber, loss);
-		// causes.getCaption().append(u1, "causes");
 
 		// Begin UI code
 		primaryStage.setMaximized(true);
 		primaryStage.setTitle("Concept Mapping");
 
-		FXMLLoader conceptMapLoader = new FXMLLoader(getClass().getResource("ConceptMapView.fxml"));
-		Pane conceptMapView = conceptMapLoader.load();
-		Scene scene = new Scene(conceptMapView);
+		ConceptMapViewBuilder builder = new ConceptMapViewBuilder();
+		Scene scene = builder.withConceptMap(conceptMap).build();
 
 		scene.setOnKeyTyped((l) -> {
 
-			if ( l.getCharacter().equals("f"))
+			if (l.getCharacter().equals("f"))
 				primaryStage.setFullScreen(true);
 		});
-		// primaryStage.setFullScreen(true);
-
-		conceptMapController = conceptMapLoader.getController();
-		// TODO the width / height values are probably off as we did not show
-		// yet
-
-		conceptMapController.addNewLinkListener(conceptMapController);
-
-		setInputPositions(scene, u1, u2, u3, u4, conceptMapView);
-
-		conceptMapController.sceneWidthProperty().bind(primaryStage.widthProperty());
-		conceptMapController.sceneHeightProperty().bind(primaryStage.heightProperty());
-
-		conceptMapController.setConceptMap(conceptMap);
 
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
-		// TUIOWrapper wrapper = new TUIOWrapper(scene,primaryStage);
-		// wrapper.start();
-		// primaryStage.setOnCloseRequest((e) -> wrapper.shutdown());
-
-	}
-
-	private void setInputPositions(Scene primaryStage, User u1, User u2, User u3, User u4, Pane conceptMapView)
-			throws IOException {
-		// north
-		final Pane v1 = addInputComponent(Position.NORTH, conceptMapView, u1);
-		v1.setRotate(180);
-		primaryStage.widthProperty().addListener((observeable, oldVal, newVal) -> {
-			v1.setLayoutX(newVal.doubleValue() * 0.6 - v1.getWidth() / 2);
-		});
-
-		// west
-		final Pane v2 = addInputComponent(Position.WEST, conceptMapView, u2);
-		v2.setRotate(90);
-
-		// as we rotate around center, we need to readjust on screen
-		primaryStage.widthProperty().addListener(c -> {
-			v2.setTranslateX(-(v2.getWidth() / 2 - v2.getHeight() / 2));
-		});
-
-		primaryStage.heightProperty().addListener((observeable, oldVal, newVal) -> {
-			v2.setLayoutY(newVal.doubleValue() * 0.5);
-		});
-
-		// south
-		final Pane v3 = addInputComponent(Position.SOUTH, conceptMapView, u3);
-		primaryStage.widthProperty().addListener((observeable, oldVal, newVal) -> {
-			v3.setLayoutX(newVal.doubleValue() * 0.5 - (v3.getWidth() / 2));
-		});
-
-		primaryStage.heightProperty().addListener((observeable, oldVal, newVal) -> {
-			v3.setLayoutY(newVal.doubleValue() - v3.getHeight());
-		});
-
-		// east
-		final Pane v4 = addInputComponent(Position.EAST, conceptMapView, u4);
-		v4.setRotate(270);
-		// as we rotate around center, we need to readjust on screen
-		primaryStage.heightProperty().addListener(c -> {
-			v4.setTranslateX((v4.getWidth() / 2 - v4.getHeight() / 2));
-		});
-		primaryStage.widthProperty().addListener((observeable, oldVal, newVal) -> {
-			v4.setLayoutX(newVal.doubleValue() - v4.getWidth());
-
-		});
-
-		primaryStage.heightProperty().addListener((observeable, oldVal, newVal) -> {
-			v4.setLayoutY(newVal.doubleValue() * 0.5);
-		});
-
-	}
-
-	private Pane addInputComponent(InputViewController.Position p, Pane conceptMapView, User u) throws IOException {
-		FXMLLoader inputLoader = new FXMLLoader(getClass().getResource("InputView.fxml"));
-		Pane inputView = inputLoader.load();
-		InputViewController inputController = inputLoader.getController();
-		inputController.setUser(u);
-		inputController.setPosition(p);
-		conceptMapView.getChildren().add(inputView);
-		inputController.addNewConceptListener(conceptMapController);
-		conceptMapController.addInputViewController(inputController);
-		return inputView;
 	}
 }
