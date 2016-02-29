b8d3c9df465c162db657bbc59e0ae3164dedaa7f
Tim Steuer <steuertim@gmail.com>
2016-02-29 19:03:09 +0100

    altered todos

--- src/test/java/de/unisaarland/edutech/conceptmapfx/ConceptMapViewTests.java
+++ src/test/java/de/unisaarland/edutech/conceptmapfx/ConceptMapViewTests.java
@@ -389,7 +389,15 @@ public class ConceptMapViewTests extends ApplicationTest {
 	}
 	// TODO too many touch points reported exception kills whole multitouch
 	// process, find out if this is a javafx bug and how to fix i
-	// TODO test for bug, create new, delete, try add new (bug already gefixt)
-	// TODO add the blinking on edit logic to links
+	
+	// TODO test for bug --> create new, delete, try add new (bug already gefixt)
+	
+	// TODO test disable new Button with various states:
+	/*
+	 *	press new -> delete new concept : new is again pressable
+	 *  press new -> add to concept : new is again pressable
+	 *  add concept -> remove all : new is NOT pressable
+	 *  add concept 1,2 --> remove all from 1,2 --> add to 1 --> new is NOT pressable  
+	 */
 	
 }
