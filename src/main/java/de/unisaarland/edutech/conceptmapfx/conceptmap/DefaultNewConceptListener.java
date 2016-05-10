f579d423e03b71f418385259696d13d5e19ea4ad
Tim Steuer <steuer.tim@gmail.com>
2016-05-10 09:46:08 +0200

    animated concept creation

--- src/main/java/de/unisaarland/edutech/conceptmapfx/conceptmap/DefaultNewConceptListener.java
+++ src/main/java/de/unisaarland/edutech/conceptmapfx/conceptmap/DefaultNewConceptListener.java
@@ -5,6 +5,10 @@ import de.unisaarland.edutech.conceptmapfx.concept.ConceptViewBuilder;
 import de.unisaarland.edutech.conceptmapfx.concept.ConceptViewController;
 import de.unisaarland.edutech.conceptmapfx.event.NewConceptListener;
 import de.unisaarland.edutech.conceptmapping.User;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
+import javafx.util.Duration;
 
 public class DefaultNewConceptListener implements NewConceptListener {
 
@@ -26,12 +30,33 @@ public class DefaultNewConceptListener implements NewConceptListener {
 
 //		conceptViewBuilder.withConceptEmptyListener(controller);
 
+
+		
 		ConceptViewController cv = conceptViewBuilder.buildControllerAndAddView(controller,
 				this.mapController.getView());
 
 		updateConceptPosition(cv);
 
 		this.mapController.add(cv);
+		
+		animateNew(cv);
+	}
+
+	private void animateNew(ConceptViewController cv) {
+		FadeTransition fadeTrans = new FadeTransition(Duration.millis(300), cv.getView());
+		fadeTrans.setFromValue(0);
+		fadeTrans.setToValue(1);
+		ScaleTransition scaleTrans = new ScaleTransition(Duration.millis(300), cv.getView());
+		scaleTrans.setFromX(0);
+		scaleTrans.setFromY(0);
+		scaleTrans.setToX(1);
+		scaleTrans.setToY(1);
+		
+		ParallelTransition transition = new ParallelTransition();
+		transition.getChildren().addAll(fadeTrans,scaleTrans);
+
+
+		transition.play();
 	}
 
 	private void updateConceptPosition(ConceptViewController cv) {
