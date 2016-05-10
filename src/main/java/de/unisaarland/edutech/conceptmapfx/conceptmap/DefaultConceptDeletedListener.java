e3dad1d2fc5a31184c8d04f44186fa2ed620324f
Tim Steuer <steuer.tim@gmail.com>
2016-05-10 09:38:45 +0200

    animation for concept deletion

--- src/main/java/de/unisaarland/edutech/conceptmapfx/conceptmap/DefaultConceptDeletedListener.java
+++ src/main/java/de/unisaarland/edutech/conceptmapfx/conceptmap/DefaultConceptDeletedListener.java
@@ -8,9 +8,14 @@ import de.unisaarland.edutech.conceptmapfx.event.ConceptDeletedListener;
 import de.unisaarland.edutech.conceptmapfx.link.LinkViewController;
 import de.unisaarland.edutech.conceptmapping.Concept;
 import de.unisaarland.edutech.conceptmapping.User;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
+import javafx.util.Duration;
 
 public class DefaultConceptDeletedListener implements ConceptDeletedListener {
 
+	private static final int FADE_DURATION = 300;
 	private ConceptMapViewController conceptMapController;
 
 	public DefaultConceptDeletedListener(ConceptMapViewController controller) {
@@ -33,9 +38,23 @@ public class DefaultConceptDeletedListener implements ConceptDeletedListener {
 	}
 
 	private void deleteConcept(ConceptViewController cv) {
-		conceptMapController.getView().remove(cv.getView());
-		conceptMapController.getMap().removeConcept(cv.getConcept());
-		conceptMapController.remove(cv);
+		FadeTransition fadeTrans = new FadeTransition(Duration.millis(FADE_DURATION), cv.getView());
+		fadeTrans.setFromValue(1);
+		fadeTrans.setToValue(0);
+		ScaleTransition scaleTrans = new ScaleTransition(Duration.millis(FADE_DURATION), cv.getView());
+		scaleTrans.setToX(0);
+		scaleTrans.setToY(0);
+		
+		ParallelTransition transition = new ParallelTransition();
+		transition.getChildren().addAll(fadeTrans,scaleTrans);
+		
+		transition.setOnFinished((e) -> {
+			conceptMapController.getView().remove(cv.getView());
+			conceptMapController.getMap().removeConcept(cv.getConcept());
+			conceptMapController.remove(cv);
+		});
+
+		transition.play();
 	}
 
 }
